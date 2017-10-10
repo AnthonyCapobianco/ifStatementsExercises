3fcd5575c50cc5362dbd5b79d02fde0376b43651
Anthony Capobianco <anth@live.be>
2017-10-11 00:39:52 +0200

    works now

--- ifStatementsExerciseFour/ifStatementsExerciseFour/main.c
+++ ifStatementsExerciseFour/ifStatementsExerciseFour/main.c
@@ -1,6 +1,6 @@
 //
 //  main.c
-//  ifStatementsExerciseOne
+//  ifStatementsExerciseFour
 //
 //  Created by Anthony Capobianco on 10/10/2017.
 //  Copyright © 2017 Anthony Capobianco. All rights reserved.
@@ -13,12 +13,12 @@ int main(void) {
     // First set i to 0, you'll forget to do it later
     int i=0;
     // A table is used to avoid redundancy in the code later on.
-    // Also an empty variable is screated to hold the result
-    float n[i],biggestNumber;
+    // Also an empty variable is created to hold the result
+    float n[3],biggestNumber;
     
-    // Ask for 3 numbers (deimals allowed)
+    // Ask for 3 numbers (decimals allowed)
     // I decided to use a while statement to avoid redundancy
-    while (i<=2) {
+    while (i<3) {
         printf("Veuillez introduire le nombre numéro %i: \n", i+1);
         scanf("%f",&n[i]);
         i++;
@@ -27,7 +27,7 @@ int main(void) {
     // Find the biggest number of the 3
     if (n[0] > n[1] && n[1] > n[2]) {
         biggestNumber = n[0];
-    }else if (n[0] > n[1] && n[2] > n[0]){
+    }else if (n[2] > n[1] && n[1] > n[0]){
         biggestNumber = n[2];
     }else{
         biggestNumber = n[1];
