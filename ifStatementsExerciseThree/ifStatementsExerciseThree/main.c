96a7d4e7cec2d9b574e43062d8cadd64a48beb40
Anthony Capobianco <anth@live.be>
2017-10-12 02:56:50 +0200

    ex 3

---
+++ ifStatementsExerciseThree/ifStatementsExerciseThree/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ifStatementsExerciseThree
+//
+//  Created by Anthony Capobianco on 12/10/2017.
+//  Copyright © 2017 Anthony Capobianco. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
